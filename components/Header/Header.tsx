faf0354e082dd613b75e17ba14324ef5a8586050
Prodi <programer972@gmail.com>
2025-02-20 17:04:55 +0100

    connect scss

---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header__inner'>
+           <h1 className='header__title'>Hello Dimon!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
