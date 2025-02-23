d96fc6024af750aac17e0f5eb68e901450a696ae
Prodi <programer972@gmail.com>
2025-02-23 14:02:05 +0100

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero/Hero"
 
 
 const Home = () => {
    return (
       <main className="main">
-        hello
+        <Hero />
       </main>
    
    )
---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import './Hero.scss'
+import { MdLocationPin } from 'react-icons/md'
+
+function Hero() {
+  return (
+    <section className='hero' id='home'>
+      <div className="hero__inner">
+        <video className='hero__video' src="./video.mp4" muted autoPlay loop></video>
+        <div className="hero__content">
+          <h3 className='hero__content-subtitle'>TRAVEL TO ANY CORNER OF THE WORLD</h3>
+          <h1 className='hero__content-title'>make your tour amazing with us</h1>
+        </div>
+
+        <div className="hero__form">
+          <h3 className='hero__form-title'>Search for Your Trip</h3>
+          <div className="hero__form-inputs">
+            
+            <div className="hero__form-block">
+              <label className='hero__form-label' htmlFor="city">Search your destination</label>
+              <div className="hero__form-inputicon">
+                <input className='hero__form-input' type="text" placeholder='Enter name here...'/>
+                <MdLocationPin className='hero__form-icon'/>
+              </div>
+            </div>
+
+            <div className="hero__form-block">
+              <label className='hero__form-label' htmlFor="date">Select you date:</label>
+              <div className="hero__form-inputicon">
+                <input className='hero__form-input' type="date" placeholder='Enter name here...'/>
+              </div>
+            </div>
+
+
+            <div className="hero__form-block">
+              <div className="hero__form-blockprice">
+                <label className='hero__form-label' htmlFor="price">Max price:</label>
+                <h4 className='hero__form-price'>$4000</h4>
+              </div>
+              <div className="hero__form-rangeblock">
+                <input className='hero__form-range' type="range" max={'5000'} min={1000}/>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
